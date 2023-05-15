073d98449b05d83825c483bcbf9f7187b657ce4c
Ralph Dahilig <ralphlance11@gmail.com>
2023-05-15 21:17:26 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const https = require("https");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs')
+
+app.get("/", function(request, response){
+    response.render('index',{ })
+});
+
+app.post("/", function(request, response){
+    const country = request.body.cityName;
+    const apiKey = "ef5647d9b2d6db965ca7f41ffd5642c9";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + country + ",ph&appid=" + apiKey +  "&units=" + unit;
+
+    https.get(url, function(res){
+        console.log(res.statusCode);
+        
+        res.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const weatherMain = weatherData.weather[0].main;
+            const weatherTemp = weatherData.main.temp;
+            const weatherDesc = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+            response.render('forecast',{mainWeather: weatherMain, tempWeather: weatherTemp, imageURL: imageURL })
+        });
+    });
+});
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server is running!");
+});
+
