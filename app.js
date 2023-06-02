c24f37fe2526b6114f479d5754ded752524a7d82
Ralph Dahilig <ralphlance11@gmail.com>
2023-06-02 20:02:48 +0800

    Fixed LEAKED API

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const app = express();
-const https = require("https");
-const bodyParser = require("body-parser");
+const express = require("express")
+const app = express()
+const https = require("https")
+const bodyParser = require("body-parser")
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
 
 app.get("/", function(request, response){
@@ -12,20 +12,20 @@ app.get("/", function(request, response){
 });
 
 app.post("/", function(request, response){
-    const country = request.body.cityName;
-    const apiKey = "ef5647d9b2d6db965ca7f41ffd5642c9";
-    const unit = "metric";
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + country + ",ph&appid=" + apiKey +  "&units=" + unit;
+    const country = request.body.cityName
+    const apiKey = process.env.WEATHER_API
+    const unit = "metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + country + ",ph&appid=" + apiKey +  "&units=" + unit
 
     https.get(url, function(res){
-        console.log(res.statusCode);
+        console.log(res.statusCode)
         
         res.on("data", function(data){
-            const weatherData = JSON.parse(data);
-            const weatherMain = weatherData.weather[0].main;
-            const weatherTemp = weatherData.main.temp;
-            const weatherDesc = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
+            const weatherData = JSON.parse(data)
+            const weatherMain = weatherData.weather[0].main
+            const weatherTemp = weatherData.main.temp
+            const weatherDesc = weatherData.weather[0].description
+            const icon = weatherData.weather[0].icon
             const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
 
             response.render('forecast',{mainWeather: weatherMain, tempWeather: weatherTemp, imageURL: imageURL })
@@ -37,6 +37,6 @@ app.post("/", function(request, response){
 
 
 app.listen(3000, function(){
-    console.log("Server is running!");
+    console.log("Server is running!")
 });
 
